7c107ae2b8ccf21e43d3d2661df18674e09827db
Deepak Patil <patilstar@gmail.com>
2010-11-10 10:04:03 +0530

    Adding intial codebase

---
+++ system/application/controllers/client.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Mundu Radio Classic
+ *
+ * API & backend development with CodeIgniter framework
+ *
+ * @package 		Mundu Radio Classic API
+ * @subpackage          CodeIgniter Controller
+ * @class		Client	
+ * @author              Deepak Patil (deepak.patil@geodesic.com) Web Enterprise Dev Team 
+ * createdate           Nov 09, 2010
+ * @copyright   	Copyright (c) 2010 - 2011, Geodesic Ltd.
+ * @link		http://troy.geodesic.net/mrc/client/
+ */
+
+class Client extends Controller {
+        function __construct()
+        {
+               parent::__construct();
+        }
+        function index()
+        {
+
+        }
+	function register()
+	{
+	
+
+	}
+	function login()
+	{
+		
+	
+	}
+	function auto_login()
+	{
+
+
+	}
+	function password()
+        {
+
+
+        }
+	function upgrade()
+	{
+
+	}
+	function download()
+	{
+
+	}
+
+}
+?>
+
---
+++ system/application/models/auth_model.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Mundu Radio Classic
+ *
+ * API & backend development with CodeIgniter framework
+ *
+ * @package             Mundu Radio Classic API
+ * @subpackage          CodeIgniter Model
+ * @class               Auth_model  
+ * @author              Deepak Patil (deepak.patil@geodesic.com) Web Enterprise Dev Team 
+ * createdate           Nov 09, 2010
+ * @copyright           Copyright (c) 2010 - 2011, Geodesic Ltd.
+ */
+
+
+class Auth_model extends Model{
+	function _constuct()
+	{
+		parent::Model();
+	}
+}
+?>
